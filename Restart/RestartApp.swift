dd60d1eb6a04a9c4c072cfe37d3039a75ac4fbec
Omar Abdulrahman <omeralhadu@icloud.com>
2023-11-27 21:26:47 +0400

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Omar Abdulrahman on 27/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
